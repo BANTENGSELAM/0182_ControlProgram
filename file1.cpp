024a307a42a1349520e97e193351409bf7993f18
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-03-09 15:39:58 +0700

    program utama

--- file1.cpp
+++ file1.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 #include <cstdlib> 
 #include <ctime>   
+using namespace std;
+//library
+
+//program utama
+int main() {
+    int x;
+    
+    srand(time(0));
+    x = rand() % 10;
+    
+    if (x == 5) {
+        x = x + 1;
+        cout << "Nilai x sudah dikoreksi menjadi : " << x << endl;
+    }
+    
+    cout << "Bilangan yang diberikan : " << x << endl;
+
+    return 0;
+}
+
