8316dcfed18cd0411ee76b6cb3ec96e35fbf56e7
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-03-09 15:49:12 +0700

    status lulus dan gagal

--- file2.cpp
+++ file2.cpp
@@ -6,3 +6,12 @@ using namespace std;
 float Rata_rata(float a, float b){
     return (a+b)/2;
 }
+
+
+//status lulus dan gagal
+string Status_lulus(float r){
+    if (r >= 60)
+        return "Lolos";
+    else
+        return "Gagal";
+}
